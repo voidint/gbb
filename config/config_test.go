569407ba6af08549d32e9e3c3bba1a00fa93d194
voidint <voidint@126.com>
2017-01-11 18:13:53 +0800

    新增config包单元测试用例

---
+++ config/config_test.go
@@ -0,0 +1,105 @@
+package config
+
+import (
+	"errors"
+	"testing"
+
+	"os"
+	"path/filepath"
+
+	"io/ioutil"
+
+	"github.com/bouk/monkey"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestLoad(t *testing.T) {
+	Convey("从磁盘文件加载配置信息", t, func() {
+		filename := filepath.Join(os.TempDir(), "gbb.json")
+		Convey("预期配置文件存在，实际文件存在", func() {
+			Convey("配置文件内容为合法JSON", func() {
+				c := Config{
+					Version: "0.1.1",
+					Tool:    "go install",
+					Package: "github.com/voidint/gbb/build",
+					Variables: []Variable{
+						{Variable: "Date", Value: "{{.Date}}"},
+						{Variable: "Commit", Value: "{{.GitCommit}}"},
+					},
+				}
+				err := Save(&c, filename)
+				So(err, ShouldBeNil)
+				defer os.Remove(filename)
+
+				loadC, err := Load(filename)
+				So(err, ShouldBeNil)
+				So(loadC, ShouldNotBeNil)
+				So(loadC.Version, ShouldEqual, c.Version)
+				So(loadC.Tool, ShouldEqual, c.Tool)
+				So(loadC.Package, ShouldEqual, c.Package)
+				So(len(loadC.Variables), ShouldEqual, len(c.Variables))
+				So(loadC.Variables[0].Variable, ShouldEqual, c.Variables[0].Variable)
+				So(loadC.Variables[0].Value, ShouldEqual, c.Variables[0].Value)
+			})
+
+			Convey("配置文件内容非合法JSON", func() {
+				So(ioutil.WriteFile(filename, []byte("hello world"), 0666), ShouldBeNil)
+				defer os.Remove(filename)
+				loadC, err := Load(filename)
+				So(err, ShouldNotBeNil)
+				So(loadC, ShouldBeNil)
+			})
+
+			Convey("读取配置文件时报错", func() {
+				var ErrRead = errors.New("failed to read file ")
+				monkey.Patch(ioutil.ReadFile, func(filename string) ([]byte, error) {
+					return nil, ErrRead
+				})
+				defer monkey.Unpatch(ioutil.ReadFile)
+				loadC, err := Load(filename)
+				So(err, ShouldNotBeNil)
+				So(err, ShouldEqual, ErrRead)
+				So(loadC, ShouldBeNil)
+			})
+		})
+
+		Convey("预期配置文件存在，实际文件不存在", func() {
+			os.Remove(filename)
+			loadC, err := Load(filename)
+			So(err, ShouldNotBeNil)
+			So(loadC, ShouldBeNil)
+		})
+	})
+}
+
+func TestSave(t *testing.T) {
+	Convey("将配置对象持久化到磁盘文件", t, func() {
+		filename := filepath.Join(os.TempDir(), "gbb.json")
+		Convey("配置对象和磁盘文件都合法", func() {
+			c := Config{
+				Version: "0.1.1",
+				Tool:    "go install",
+				Package: "github.com/voidint/gbb/build",
+				Variables: []Variable{
+					{Variable: "Date", Value: "{{.Date}}"},
+					{Variable: "Commit", Value: "{{.GitCommit}}"},
+				},
+			}
+			So(Save(&c, filename), ShouldBeNil)
+			defer os.Remove(filename)
+
+			loadC, err := Load(filename)
+			So(err, ShouldBeNil)
+			So(loadC, ShouldNotBeNil)
+			So(loadC.Version, ShouldEqual, c.Version)
+			So(loadC.Tool, ShouldEqual, c.Tool)
+			So(loadC.Package, ShouldEqual, c.Package)
+			So(len(loadC.Variables), ShouldEqual, len(c.Variables))
+			So(loadC.Variables[0].Variable, ShouldEqual, c.Variables[0].Variable)
+			So(loadC.Variables[0].Value, ShouldEqual, c.Variables[0].Value)
+		})
+		Convey("配置对象为nil", func() {
+			So(Save(nil, filename), ShouldBeNil)
+		})
+	})
+}
