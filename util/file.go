9841b9f9c78685802b76aac902358cd1d68b6043
voidint <voidint@126.com>
2017-01-11 18:14:34 +0800

    新增util包单元测试用例

--- util/file.go
+++ util/file.go
@@ -2,7 +2,11 @@ package util
 
 import "os"
 
+// FileExist 判断指定路径的文件是否存在
 func FileExist(filename string) bool {
-	_, err := os.Stat(filename)
-	return !(err != nil && os.IsNotExist(err))
+	info, err := os.Stat(filename)
+	if err != nil {
+		return os.IsExist(err)
+	}
+	return !info.IsDir()
 }
